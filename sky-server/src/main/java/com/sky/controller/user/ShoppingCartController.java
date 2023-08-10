1d847ff76bbf49144082a5354ebcab689c8728e8
zc <zc@126.com>
2023-08-10 10:08:28 +0800

    完善购物车

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -6,21 +6,21 @@ import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * @author zc
- * @create 2023-08-09-16:52
- */
 @RestController
 @RequestMapping("/user/shoppingCart")
-@Api(tags = "c端购物车相关接口")
+@Slf4j
+@Api(tags = "C端购物车相关接口")
 public class ShoppingCartController {
+
     @Autowired
     private ShoppingCartService shoppingCartService;
+
     /**
      * 添加购物车
      * @param shoppingCartDTO
@@ -29,6 +29,7 @@ public class ShoppingCartController {
     @PostMapping("/add")
     @ApiOperation("添加购物车")
     public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("添加购物车，商品信息为：{}",shoppingCartDTO);
         shoppingCartService.addShoppingCart(shoppingCartDTO);
         return Result.success();
     }
@@ -40,7 +41,7 @@ public class ShoppingCartController {
     @GetMapping("/list")
     @ApiOperation("查看购物车")
     public Result<List<ShoppingCart>> list(){
-        List<ShoppingCart> list=shoppingCartService.showShoppingCart();
+        List<ShoppingCart> list = shoppingCartService.showShoppingCart();
         return Result.success(list);
     }
 
@@ -52,6 +53,19 @@ public class ShoppingCartController {
     @ApiOperation("清空购物车")
     public Result clean(){
         shoppingCartService.cleanShoppingCart();
+        return Result.success();
+    }
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     * @return
+     */
+    @PostMapping("/sub")
+    @ApiOperation("删除购物车中一个商品")
+    public Result sub(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("删除购物车中一个商品，商品：{}", shoppingCartDTO);
+        shoppingCartService.subShoppingCart(shoppingCartDTO);
         return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -5,16 +5,28 @@ import com.sky.entity.ShoppingCart;
 
 import java.util.List;
 
-/**
- * @author zc
- * @create 2023-08-09-16:57
- */
 public interface ShoppingCartService {
-    //添加购物车
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
-    //查看购物车
+
+    /**
+     * 查看购物车
+     * @return
+     */
     List<ShoppingCart> showShoppingCart();
 
-    //清空购物车
+    /**
+     * 清空购物车
+     */
     void cleanShoppingCart();
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
